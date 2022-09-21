c77705fa01f54e4787ebcf2a67412dc6e7f20b7a
Alex <a1980dominov@gmail.com>
2022-09-21 21:32:32 +0300

    first commit

---
+++ screen.js
@@ -0,0 +1,32 @@
+let magicButton=document.getElementById('magic')
+let priceOne=document.getElementById('price-one')
+let priceTwo=document.getElementById('price-two')
+let priceThree=document.getElementById('price-three')
+let textBurger=document.getElementById('textBurger')
+let burger=document.getElementById('burger')
+priceOne.innerText='$19 /month'
+priceTwo.innerText='$59 /month'
+priceThree.innerText='$89 /month'
+let itemBank=1
+let counter=1
+magicButton.onclick=function(){
+    itemBank++
+    choisePrace()
+    return itemBank%2==0 ? priceOne.innerText='$10 /yearly':
+    priceOne.innerText='$19 /month'
+    
+}
+function choisePrace(){
+    choisePraceThree()
+    return itemBank%2==0 ? priceTwo.innerText='$30 /yearly':
+    priceTwo.innerText='$59 /month'
+}
+function choisePraceThree(){
+    return itemBank%2==0 ? priceThree.innerText='$50 /yearly':
+    priceThree.innerText='$89 /month'
+}
+burger.onclick=function(){
+    counter++
+    let list="<li><ul>Products</ul><ul>Plans</ul><ul>Contacts</ul></li>"
+    return  counter%2==0 ? textBurger.innerHTML=list:textBurger.innerText=""
+}
