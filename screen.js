e149e084f1439479e5d68d61f4500b260a7a3b60
Alex <a1980dominov@gmail.com>
2022-09-21 22:42:52 +0300

    added timer

--- screen.js
+++ screen.js
@@ -4,11 +4,14 @@ let priceTwo=document.getElementById('price-two')
 let priceThree=document.getElementById('price-three')
 let textBurger=document.getElementById('textBurger')
 let burger=document.getElementById('burger')
+let timerButton=document.getElementById('timer')
+let timerScreen=document.getElementById('timer-screen')
 priceOne.innerText='$19 /month'
 priceTwo.innerText='$59 /month'
 priceThree.innerText='$89 /month'
 let itemBank=1
 let counter=1
+let timeFunction=1
 magicButton.onclick=function(){
     itemBank++
     choisePrace()
@@ -29,4 +32,12 @@ burger.onclick=function(){
     counter++
     let list="<li><ul>Products</ul><ul>Plans</ul><ul>Contacts</ul></li>"
     return  counter%2==0 ? textBurger.innerHTML=list:textBurger.innerText=""
+}
+timerButton.onclick=function(){
+  setInterval(timeNow,1000)
+  timeFunction++
+  function timeNow(){
+   return timeFunction%2==0 ? timerScreen.innerText="Today: " +new Date().toLocaleTimeString() :
+   timerScreen.innerText=''
+  }
 }
